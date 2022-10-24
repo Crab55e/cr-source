5fa0a33c519eaa6b247a60737ee1526d961810e1
Mets-server <100203673+Mets-server@users.noreply.github.com>
2022-10-24 20:26:57 +0900

    忘れたけどとりまコミット

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,10 @@ $(".line .circle").on("click", function() {
 document.addEventListener('mousemove', function(e) {
     stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
 });
+
+
+window.addEventListener('scroll', function() {
+    if (396 <= window.scrollY) {
+        $(".about").css("visibility", "visible !important")
+    }
+});
