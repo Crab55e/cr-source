911e7809cc6fcc00f68ecd35bf598972c23144a6
Mets-server <100203673+Mets-server@users.noreply.github.com>
2022-11-08 06:56:02 +0900

    コミットに名前つけるのめんどすぎだろ！

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,17 @@ $(".line .circle").on("click", function() {
     $("html").append("<div id=\"stalker\"></div>")
     const stalker = document.getElementById('stalker');
 });
+
 document.addEventListener('mousemove', function(e) {
     stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
 });
 
+$(".about").css("visibility", "hidden")
 
-window.addEventListener('scroll', function() {
-    if (396 <= window.scrollY) {
-        $(".about").css("visibility", "visible !important")
-    }
+ScrollReveal().reveal('.about', {
+    duration: 1200,
+    origin: 'bottom',
+    viewFactor: 0.25,
+    reset: false,
+    distance: '200px'
 });
---
+++ scripts/sleep.js
@@ -0,0 +1,1 @@
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
