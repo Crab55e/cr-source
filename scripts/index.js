f4b817f0632465a736b8c0b60b123e7777b33c11
Mets-server <100203673+Mets-server@users.noreply.github.com>
2022-12-30 18:43:32 +0900

    忘れたけどなんかコミットしてなかったからこみっと

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,27 @@
-$(".line .circle").on("click", function() {
+if (document.cookie.indexOf("circleClicked") == -1) {
+    $(".line .circle").addClass("clickable");
+} else {
+    console.log("circle is already clicked")
+    $("#stalker").css("visibility", "hidden");
+    $("html").append("<div id=\"stalker\"></div>")
+};
+$(".line .circle.clickable").on("click", function() {
     $(".line .circle").remove();
     $("html").append("<div id=\"stalker\"></div>")
     const stalker = document.getElementById('stalker');
-});
-
+})
 document.addEventListener('mousemove', function(e) {
     stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
 });
 
 $(".about").css("visibility", "hidden")
 
-ScrollReveal().reveal('.about', {
-    duration: 1200,
+ScrollReveal().reveal('.panel', {
+    duration: 400,
     origin: 'bottom',
-    viewFactor: 0.25,
+    viewFactor: 0.5,
     reset: false,
     distance: '200px'
 });
+
+document.cookie = "circleClicked = true";
--- scripts/mouse-stalker.js
+++ scripts/mouse-stalker.js
@@ -4,3 +4,13 @@ const cursor = document.getElementById('cursor');
 document.addEventListener('mousemove', function(e) {
     cursor.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
 });
+
+$(".red").hover(function() {
+    $("#stalker").css("visibility", "hidden");
+    $("#cursor").css("visibility", "visible");
+});
+
+$(".unred").hover(function() {
+    $("#stalker").css("visibility", "visible");
+    $("#cursor").css("visibility", "hidden");
+})
